8185d1b7edf02b495ea20bcc1d0fb1d00de11830
billsmoke <dumanbs@gmail.com>
2024-06-23 12:30:57 +0300

    ui fixes

--- Assets/MetalSimulationAssets/Scripts/ShootBalls.cs
+++ Assets/MetalSimulationAssets/Scripts/ShootBalls.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class ShootBalls : MonoBehaviour
@@ -17,13 +18,29 @@ public class ShootBalls : MonoBehaviour
 
     public void OnClick(InputAction.CallbackContext context)
     {
-        Ray mouseRay = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
-        GameObject newGameObject = Instantiate(prefabToInstantiate, mouseRay.origin, Quaternion.identity);
-        Rigidbody rb = newGameObject.GetComponent<Rigidbody>();
- 
-        if (rb != null)
+        StartCoroutine(CheckClickUI());
+    }
+    
+    private IEnumerator CheckClickUI()
+    {
+        // Wait until the end of the frame
+        yield return new WaitForEndOfFrame();
+
+        if (EventSystem.current.IsPointerOverGameObject())
         {
-            rb.velocity = mouseRay.direction * speed;
+            //Debug.Log("pointer over ui do nothing");
+        }
+        else
+        {
+            Ray mouseRay = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+            GameObject newGameObject = Instantiate(prefabToInstantiate, mouseRay.origin, Quaternion.identity);
+            Rigidbody rb = newGameObject.GetComponent<Rigidbody>();
+ 
+            if (rb != null)
+            {
+                rb.velocity = mouseRay.direction * speed;
+            }
         }
     }
+    
 }
