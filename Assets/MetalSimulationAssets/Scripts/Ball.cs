c62918a1a61a7688bd136e7bb556a78c93c422fe
billsmoke <dumanbs@gmail.com>
2024-06-23 07:37:42 +0300

    cpu deformation

--- Assets/MetalSimulationAssets/Scripts/Ball.cs
+++ Assets/MetalSimulationAssets/Scripts/Ball.cs
@@ -6,33 +6,13 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    private Rigidbody rb;
     private void OnEnable()
     {
-        rb = gameObject.GetComponent<Rigidbody>();
         Destroy(gameObject,15);
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.CompareTag("Deformable"))
-        {
-            ContactPoint contactPoint = other.GetContact(0);
-            //Debug.Log(contactPoint.point);
-        
-            PlasticDeformationController controller = other.gameObject.GetComponent<PlasticDeformationController>();
-        
-            controller.impactPoint = contactPoint.point;
-            controller.impactVector = rb.velocity;
-        
-            //Debug.Log(controller.impactVector);
-        
-            controller.OnGPUCall();
-            Destroy(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+        Destroy(gameObject);
     }
 }
--- Assets/MetalSimulationAssets/Scripts/PlasticDeformationController.cs
+++ Assets/MetalSimulationAssets/Scripts/PlasticDeformationController.cs
@@ -8,23 +8,62 @@ using Random = UnityEngine.Random;
 public class PlasticDeformationController : MonoBehaviour
 {
     [SerializeField] private ComputeShader plasticDeformationShader;
-    [SerializeField] private Mesh mesh;
     [SerializeField] private MeshCollider meshCollider;
-    [SerializeField] private Vector3[] vertices;
+    [SerializeField] private Vector3[] vertices; 
+    [SerializeField] private Vector3[] defaultVertices;
+
+    [Range(0,10)]
+    [SerializeField] private float deformRadius = 0.2f;
+    [Range(0,10)]
+    [SerializeField] private float maxDeform = 0.1f;
+    [Range(0,1)]
+    [SerializeField] private float damageFalloff = 1.0f;
+    [Range(0,10)]
+    [SerializeField] private float damageMultiplier = 1.0f;
+    [Range(0,100000)] //this will be hardness???
+    public float minDamage = 1.0f;
     
+    
+    public bool useGPU = false;
     public Vector3 impactPoint;
     public Vector3 impactVector;
     
+    private MeshFilter meshFilter;
 
     private void Start()
     {
-        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        meshFilter = GetComponent<MeshFilter>();
         meshCollider = GetComponent<MeshCollider>();
-        mesh = meshFilter.mesh;
-        vertices = mesh.vertices;
+        vertices = meshFilter.mesh.vertices;
+        defaultVertices = vertices;
     }
-    
-    public void OnGPUCall()
+
+    private void OnCollisionEnter(Collision other)
+    {
+        float collisionPower = other.impulse.magnitude;
+        
+        //Debug.Log(collisionPower);
+        
+        if (collisionPower > minDamage)
+        {
+            foreach (ContactPoint contactPoint in other.contacts)
+            {
+                if (useGPU)
+                {
+                    impactPoint = contactPoint.point;
+                    impactVector = other.gameObject.GetComponent<Rigidbody>().velocity;
+                    OnGPUCall();
+                }
+                else
+                {
+                    impactPoint = transform.InverseTransformPoint(contactPoint.point);
+                    OnCPUCall();
+                }
+            }
+        }
+    }
+
+    private void OnGPUCall()
     {
         int vector3Size = sizeof(float) * 3;
         int totalSize = vector3Size;
@@ -42,14 +81,44 @@ public class PlasticDeformationController : MonoBehaviour
         vertexBuffer.GetData(vertices);
         
         //do the mesh update
-        mesh.SetVertices(vertices);
-        mesh.RecalculateBounds();
-        mesh.RecalculateNormals();
-        mesh.RecalculateTangents();
-        //meshCollider.sharedMesh = mesh;
+        meshFilter.mesh.vertices = vertices;
+        meshFilter.mesh.RecalculateBounds();
+        meshFilter.mesh.RecalculateNormals();
+        meshFilter.mesh.RecalculateTangents();
+        meshCollider.sharedMesh = meshFilter.mesh;
         
         vertexBuffer.Dispose();
         
+    }
+
+    private void OnCPUCall()
+    {
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 vertexPosition = vertices[i];
+            float distanceFromCollision = Vector3.Distance(vertexPosition, impactPoint);
+            float distanceFromOriginal = Vector3.Distance(defaultVertices[i], vertexPosition);
+
+            if (distanceFromCollision < deformRadius && distanceFromOriginal < maxDeform)
+            {
+                float falloff = 1 - (distanceFromCollision / deformRadius) * damageFalloff;
+
+                float xDeform = impactPoint.x * falloff;
+                float yDeform = impactPoint.y * falloff;
+                float zDeform = impactPoint.z * falloff;
+
+                xDeform = Mathf.Clamp(xDeform, 0, maxDeform);
+                yDeform = Mathf.Clamp(yDeform, 0, maxDeform);
+                zDeform = Mathf.Clamp(zDeform, 0, maxDeform);
+
+                Vector3 deform = new Vector3(xDeform, yDeform, zDeform);
+                vertices[i] -= deform * damageMultiplier;
+            }
+        }
+        //apply changes
+        meshFilter.mesh.vertices = vertices;
+        meshCollider.sharedMesh = meshFilter.mesh;
+
     }
 
     private void OnGUI()
