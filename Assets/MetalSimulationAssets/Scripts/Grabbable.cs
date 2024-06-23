59dc3b22f4cd02e078600a5c87eb81c44b67cd27
billsmoke <dumanbs@gmail.com>
2024-06-23 16:01:25 +0300

    indicator sphere done

--- Assets/MetalSimulationAssets/Scripts/Grabbable.cs
+++ Assets/MetalSimulationAssets/Scripts/Grabbable.cs
@@ -2,25 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-struct GrabSphere
-{
-    private Vector3 position;
-    private float radius;
-}
-
 public class Grabbable : MonoBehaviour
 {
     [SerializeField] private ComputeShader grabShader;
+    [SerializeField] private Vector3[] vertices; 
+    [SerializeField] private Vector3[] defaultVertices;
+    [SerializeField] private Vector3 grabPosition;
+    [SerializeField] private float grabRadius;
+    
+    [Range(0,10)]
+    [SerializeField] private float maxDeform = 0.1f;
+    [Range(0,10)]
+    [SerializeField] private float maxElasticity = 0.1f;
+    
+    
+    private MeshFilter meshFilter;
+    private ComputeBuffer vertexBuffer;
+    private ComputeBuffer defaultVertexBuffer;
     
     // Start is called before the first frame update
     void Start()
     {
+        meshFilter = GetComponent<MeshFilter>();
+        vertices = meshFilter.mesh.vertices;
+        defaultVertices = meshFilter.mesh.vertices;
         
+        //sends default vertex data to be used in calculations without change
+        int vector3Size = sizeof(float) * 3;
+        int totalSize = vector3Size;
+        defaultVertexBuffer = new ComputeBuffer(defaultVertices.Length, totalSize);
+        defaultVertexBuffer.SetData(defaultVertices);
+        grabShader.SetBuffer(0,"defaultVertices", defaultVertexBuffer);
+        defaultVertexBuffer.Dispose();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    void UpdateMesh()
+    {
+        meshFilter.mesh.vertices = vertices;
+        //meshFilter.mesh.RecalculateBounds();
+        //meshFilter.mesh.RecalculateNormals();
+        //meshFilter.mesh.RecalculateTangents();
     }
 }
--- Assets/MetalSimulationAssets/Scripts/SphereGrabber.cs
+++ Assets/MetalSimulationAssets/Scripts/SphereGrabber.cs
@@ -1,29 +1,66 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class SphereGrabber : MonoBehaviour
 {
+    [SerializeField] private InputAction mouseClick;
     [SerializeField] private bool canGrab = false;
+    [SerializeField] private GameObject sphere;
+    [SerializeField] private Vector3 worldPosition;
+    [SerializeField] private float mouseDragSpeed = .05f;
+
+    private Vector3 velocity = Vector3.zero;
+    
     
     //just follows the mouse position and projects a sphere at that point. if player clicks, it sends that sphere data to compute shader
     //and uses it to transform vertices, create new ones if rupture happens
     
-    
+    [SerializeField] private Camera mainCamera;
 
-    // Update is called once per frame
-    private void Update()
+    private void Awake()
     {
-        if (canGrab) //do stuff
-        {
-            
-        }
+        mainCamera = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        mouseClick.Enable();
+        mouseClick.performed += MousePressed;
+    }
+    
+    void Update()
+    {
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+        mousePos.z = Camera.main.nearClipPlane + 5.0f;
+        worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+        sphere.transform.position = Vector3.SmoothDamp(sphere.transform.position, worldPosition, ref velocity, mouseDragSpeed);
+    }
+
+    private void OnDisable()
+    {
+        mouseClick.performed -= MousePressed;
+        mouseClick.Disable();
+    }
+
+    private void MousePressed(InputAction.CallbackContext context)
+    {
+        //mouse pressed call compute shader here
     }
 
     public void ToggleGrab()
     {
         canGrab = !canGrab;
+        if (canGrab)
+        {
+            //create a sphere in worldPosition
+            sphere = Instantiate(sphere, worldPosition, quaternion.identity);
+        }
     }
     
 }
